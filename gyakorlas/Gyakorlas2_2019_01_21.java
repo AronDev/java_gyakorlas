6753a5c27d3c248050f3e4695b5eb88c37af8253
Kovács Áron György <zipike02@gmail.com>
2019-09-05 19:33:51 +0200

    tömb

--- gyakorlas/Gyakorlas2_2019_01_21.java
+++ gyakorlas/Gyakorlas2_2019_01_21.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class Gyakorlas2_2019_01_21 {
     public static void main(String[] args) {
         String be = null;
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Add meg a szöveget: ");
-        
+
         be = sc.next();
-        
-        char t[] = {'a','á','e','é','i','í','o','ó','ö','ő','u','ú','ü','ű'};
-        
+
+        char [] t = {'a','á','e','é','i','í','o','ó','ö','ő','u','ú','ü','ű'};
+
         for(int i = 0; i < be.length(); i++) {
             for(int j = 0; j < t.length-1; j++) {
                 if(be.charAt(i) == t[j]) {
