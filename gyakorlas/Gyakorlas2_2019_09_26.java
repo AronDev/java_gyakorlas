dc5c4249f8afac5d6e55a633f8bd8c9a76aa058d
Aron Kovacs <45430204+AronDev@users.noreply.github.com>
2019-09-26 10:17:07 +0200

    Anagramma ellenőrzés

---
+++ gyakorlas/Gyakorlas2_2019_09_26.java
@@ -0,0 +1,41 @@
+/*
+ * Anagramma ellenőrzése
+ */
+
+import java.util.Scanner;
+
+public class Gyakorlas2_2019_09_26 {
+    public static void main(String[] args) {
+        // Változók
+        Scanner sc = new Scanner(System.in);
+        String reversedStr = "";
+        String inputStr = "";
+        
+        // Bekérés
+        inputStr = sc.nextLine();
+        
+        // Bekért adat átalakítása
+        inputStr = atalakitas(inputStr);
+        
+        // Műveletek
+        for(int i = inputStr.length()-1; i >= 0; i--) {
+            reversedStr += Character.toString(inputStr.charAt(i));
+        }
+        
+        // Ellenőrzés
+        System.out.printf("A szó %sanagramma!%n", reversedStr.equals(inputStr) ? "" : "nem ");
+    }
+    private static String atalakitas(String iStr) {
+        String returnStr = iStr.trim();
+        returnStr = returnStr.replaceAll("\\.", "");
+        returnStr = returnStr.replaceAll(",", "");
+        returnStr = returnStr.replaceAll("-", "");
+        returnStr = returnStr.replaceAll("\\+", "");
+        returnStr = returnStr.replaceAll("\\?", "");
+        returnStr = returnStr.replaceAll("\\!", "");
+        returnStr = returnStr.replaceAll("\\*", "");
+        returnStr = returnStr.replaceAll("_", "");
+        returnStr = returnStr.toLowerCase();
+        return returnStr;
+    }
+}
