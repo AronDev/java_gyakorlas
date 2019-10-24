578276470e3239c9a6931bbfabad542a4f256f4f
Aron Kovacs <45430204+AronDev@users.noreply.github.com>
2019-10-24 10:21:40 +0200

    Add files via upload

---
+++ gyakorlas/Gyakorlas1_2019_10_24.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+import javax.swing.JOptionPane;
+
+public class Gyakorlas1_2019_10_24 {
+    // Változók és konstansok
+    static final int N = 20;
+    static final String [] users = {"Aladár", "Bendegúz"};
+    static int [] data0 = new int[N];
+    static int [] data1 = new int[N];
+    
+    public static void main(String[] args) {
+        readData("files/aladar.txt", data0, 0);
+        readData("files/bendeguz.txt", data1, 1);
+        printData(data0);
+        printData(data1);
+        System.out.print(printData_biggerDropCount(data0, data1, 0, 1));
+        System.out.print(printData_biggerDropCount(data1, data0, 1, 0));
+        System.out.print(printData_number6DropCount(data0, 0));
+        System.out.print(printData_number6DropCount(data1, 1));
+        System.out.printf("%snak %svolt nyerő szériája %s ellen.\n", users[0], !getData_winningSeries(data0, data1) ? "nem" : "", users[1]);
+    }
+    public static void readData(String fileName, int [] targetArray, int user) {
+        File file = new File(fileName);
+        String lineStr = "";
+        int j = 0;
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while(sc.hasNextLine()) {
+                lineStr = sc.nextLine();
+                targetArray[j++] = Integer.parseInt(lineStr);
+            }
+        } catch(FileNotFoundException fnfe) {
+            showMsg(JOptionPane.ERROR_MESSAGE, "Nincs ilyen fájl!", fnfe.getMessage());
+        } finally {
+            if(sc != null) sc.close();
+            System.out.printf("%s eredményei sikeresen beolvasva(%s)\n", users[user], fileName);
+        }
+    }
+    public static void printData(int [] sourceArray) {
+        for(int i = 0; i < N; i++) {
+            System.out.printf("%d, ", sourceArray[i]);
+        }
+        System.out.println();
+    }
+    public static String printData_biggerDropCount(int [] sourceArray1, int [] sourceArray2, int user1, int user2) {
+        String finalStr = "";
+        int sA1_biggerDrops = 0; // sA1 hányszor dobott többet mint sA2
+        for(int i = 0; i < N; i++) {
+            if(sourceArray1[i] > sourceArray2[i]) sA1_biggerDrops ++;
+        }
+        finalStr += String.format("%s %d alkalommal dobott többet mint %s.\n", users[user1], sA1_biggerDrops, users[user2]);
+        return finalStr;
+    }
+    public static String printData_number6DropCount(int [] sourceArray, int user) {
+        String finalStr = "";
+        int sA_number6Drops = 0;
+        for(int i = 0; i < N; i++) {
+            if(sourceArray[i] == 6) sA_number6Drops ++;
+        }
+        finalStr += String.format("%s %d alkalommal dobott hatost.\n", users[user], sA_number6Drops);
+        return finalStr;
+    }
+    public static boolean getData_winningSeries(int [] sA1, int [] sA2) {
+        int j = 0;
+        while(j < N-2 && !(sA1[j] > sA2[j] && sA1[j+1] > sA2[j+1] && sA1[j+2] > sA2[j+2])) {
+            return true;
+        }
+        return false;
+    }
+    public static void showMsg(int msgType, String msgTitle, String msgText) {
+        JOptionPane.showMessageDialog(null, msgText, msgTitle, msgType);
+    }
+}
