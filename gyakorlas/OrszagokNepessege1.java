55cc4336ab83ece0c58decd53143e55f7e13a152
Aron Kovacs <45430204+AronDev@users.noreply.github.com>
2019-11-14 11:40:16 +0100

    Update OrszagokNepessege1.java

--- gyakorlas/OrszagokNepessege1.java
+++ gyakorlas/OrszagokNepessege1.java
@@ -13,6 +13,8 @@ public class OrszagokNepessege1 {
     
     public static void main(String[] args) {
         beolvasas("files/orszagok.txt");
+        feladat1();
+        feladat2();
     }
 
     private static void beolvasas(String fileName) {
@@ -20,6 +22,7 @@ public class OrszagokNepessege1 {
         try(Scanner sc = new Scanner(file)) {
             String sor;
             String [] tordeles;
+            System.out.println("[I] Beolvasás megkezdve..");
             while(sc.hasNextLine()) {
                 sor = sc.nextLine();
                 tordeles = sor.split("\t");
@@ -29,10 +32,41 @@ public class OrszagokNepessege1 {
                         tordeles[2],                    // országnév
                         Integer.parseInt(tordeles[3]),  // függőség
                         Long.parseLong(tordeles[4]));   // népesség
-                System.out.println(orszag.kiiras());
+                orszagLista.add(orszag);
             }
+            System.out.println("[I] Beolvasás sikeres!");
         } catch(FileNotFoundException fnfe) {
-            System.err.println(fnfe.getMessage());
+            System.out.println("[E] " + fnfe.getMessage());
         } 
+    }
+
+    private static void feladat1() {
+        /* Melyik a legnépesebb ország? */
+        int max = 0;
+        for(int i = 0; i < orszagLista.size(); i++) {
+            if(orszagLista.get(i).nepesseg > orszagLista.get(max).nepesseg) max = i;
+        }
+        System.out.printf("[I] Legnépesebb ország: %s (%,d fő)\n", orszagLista.get(max).orszagnev, orszagLista.get(max).nepesseg);
+    }
+
+    private static void feladat2() {
+        /* Melyik a legnépesebb függő terület? */
+        int maxFuggoseg = 0;
+        int a = -1;
+        int b = 0;
+        for(int i = 0; i < orszagLista.size(); i++) {
+            if(orszagLista.get(i).fuggoseg > 0) {
+                if(a == -1) {
+                    a = i;
+                }
+                b = i;
+            }
+        }
+        System.out.printf("a: %d | b: %d\n", a, b);
+        for(int i = a; i < b; i++) {
+            if(orszagLista.get(i).nepesseg > orszagLista.get(maxFuggoseg).nepesseg)
+                maxFuggoseg = i;
+        }
+        System.out.printf("[I] Legnépesebb függő ország: %s (%,d fő)\n", orszagLista.get(maxFuggoseg).orszagnev, orszagLista.get(maxFuggoseg).nepesseg);
     }
 }
