3a3914822891f31dc0dceb90700c514f5a2c03f1
Aron Kovacs <45430204+AronDev@users.noreply.github.com>
2019-11-28 18:31:38 +0100

    Add files via upload

--- gyakorlas/Gyakorlas1_2019_11_28/Functions.java
+++ gyakorlas/Gyakorlas1_2019_11_28/Functions.java
@@ -3,6 +3,8 @@ package Gyakorlas1_2019_11_28;
 //import static Gyakorlas1_2019_11_28.Main.sportagLista;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
@@ -17,29 +19,33 @@ public class Functions {
     }
     
     static void systemMsg(int msgType, String msgText) {
-        switch(msgType) {
-            case 0: {
-                System.out.printf("[I] %s\n", msgText);
-                break;
-            } case 1: {
-                System.out.printf("[W] %s\n", msgText);
-                break;
-            } case 2: {
-                System.out.printf("[E] %s\n", msgText);
-                break;
-            } case 3: {
-                if(Main.debug) {
-                    System.out.printf("[D] %s\n", msgText);
+        if(msgText.trim().length() > 0) {
+            switch(msgType) {
+                case 0: {
+                    System.out.printf("[I] %s\n", msgText);
                     break;
+                } case 1: {
+                    System.out.printf("[W] %s\n", msgText);
+                    break;
+                } case 2: {
+                    System.out.printf("[E] %s\n", msgText);
+                    break;
+                } case 3: {
+                    if(Main.DEBUG) {
+                        System.out.printf("[D] %s\n", msgText);
+                    }
+                    break;
+                } default: {
+                    systemMsg(ERROR_MSG, String.format("Hibás msgType érték a systemMsg funkcióban! (msgType = %d)", msgType));
                 }
-            } default: {
-                System.out.println("[E] Invalid msgType at systemMsg function!");
             }
+        } else {
+            systemMsg(ERROR_MSG, "msgTextnek nincsenk értéke a systemMsg funkcióban!");
         }
     }
     
     static void readFile(String fileName) {
-        systemMsg(INFO_MSG, String.format("Fájl beolvasása megkezdve.. (%s)", fileName));
+        systemMsg(DEBUG_MSG, String.format("Fájl beolvasása megkezdve.. (%s)", fileName));
         File f = new File(fileName);
         try(Scanner sc = new Scanner(f)) {
             String line;
@@ -52,7 +58,7 @@ public class Functions {
                     Datastores.sportagLista.get(Datastores.sportagLista.size() - 1).donto[i-1] = Integer.parseInt(elements[i]);
                 }
             }
-            systemMsg(INFO_MSG, String.format("Fájl beolvasva! (%s)", fileName));
+            systemMsg(DEBUG_MSG, String.format("Fájl beolvasva! (%s)", fileName));
         } catch(FileNotFoundException fnfe) {
             systemMsg(ERROR_MSG, String.format("%s", fnfe.getMessage()));
         }
--- gyakorlas/Gyakorlas1_2019_11_28/Main.java
+++ gyakorlas/Gyakorlas1_2019_11_28/Main.java
@@ -1,12 +1,18 @@
 package Gyakorlas1_2019_11_28;
 
+
 public class Main {   
-    static final boolean debug = true;
+    static final boolean DEBUG = false;
     
     public static void main(String[] args) {
+        Functions.systemMsg(Functions.DEBUG_MSG, "Debug bekapcsolva");
+        
         Functions.readFile("files/London2012.txt");
+        
         feladat2();
         feladat3();
+        feladat4();
+        feladat5();
     }
 
     private static void feladat2() {
@@ -25,21 +31,53 @@ public class Main {
                 if(Datastores.sportagLista.get(sorszam).donto[i] > 0) db ++;
             }
             
-            Functions.systemMsg(Functions.INFO_MSG, "Döntős napok száma " + keresettSportag + " sportágban: " + db + " db");
+            Functions.systemMsg(Functions.INFO_MSG, String.format("Döntős napok száma %s sportágban: %d db", keresettSportag.toLowerCase(), db));
         } else Functions.systemMsg(Functions.WARN_MSG, String.format("Nincs ilyen sportág! (%s)", keresettSportag));
     }
 
     private static void feladat3() {
         Functions.systemMsg(Functions.INFO_MSG, "=== [3. feladat] ===");
-        int aranyermek = 0;
+        
+        int aranyermekSzama = 0;
         final String keresettSportag = "Úszás";
         for(int i = 0; i < Datastores.sportagLista.size(); i++) {
             if(Datastores.sportagLista.get(i).nev.contentEquals(keresettSportag)) {
                 for(int j = 0; j < Datastores.sportagLista.get(i).donto.length; j++) {
-                    aranyermek += Datastores.sportagLista.get(i).donto[j];
+                    aranyermekSzama += Datastores.sportagLista.get(i).donto[j];
                 }
             }
         }
-        Functions.systemMsg(Functions.INFO_MSG, "Aranyérmek száma " + keresettSportag + " sportágban: " + aranyermek + " db");
+        Functions.systemMsg(Functions.INFO_MSG, String.format("Aranyérmek száma %s sportágban: %d db", keresettSportag.toLowerCase(), aranyermekSzama));
+    }
+
+    private static void feladat4() {
+        Functions.systemMsg(Functions.INFO_MSG, "=== [4. feladat] ===");
+        
+        int [] dontokSzama = new int[Datastores.sportagLista.get(Datastores.sportagLista.size()-1).donto.length];
+        for(int i = 0; i < Datastores.sportagLista.size(); i++) {
+            for(int j = 0; j < dontokSzama.length; j++) {
+                if(Datastores.sportagLista.get(i).donto[j] > 0) dontokSzama[j] += Datastores.sportagLista.get(i).donto[j];
+            }
+        }
+        
+        int max = 0;
+        for(int i = 0; i < dontokSzama.length; i++) {
+            if(dontokSzama[i] > dontokSzama[max]) max = i;
+        }
+        
+        Functions.systemMsg(Functions.INFO_MSG, String.format("A legtöbb döntő (%s db) %d-án/én volt", dontokSzama[max], max));
+    }
+
+    private static void feladat5() {
+        Functions.systemMsg(Functions.INFO_MSG, "=== [5. feladat] ===");
+        
+        int aranyermekSzama = 0;
+        for(int i = 0; i < Datastores.sportagLista.size(); i++) {
+            for(int j = 0; j < Datastores.sportagLista.get(Datastores.sportagLista.size()-1).donto.length; j++) {
+                if(Datastores.sportagLista.get(i).donto[j] > 0) aranyermekSzama += Datastores.sportagLista.get(i).donto[j];
+            }
+        }
+        
+        Functions.systemMsg(Functions.INFO_MSG, String.format("%d db aranyérmet osztottak ki az olimpián", aranyermekSzama));
     }
 }
