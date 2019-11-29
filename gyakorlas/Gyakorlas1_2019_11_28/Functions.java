c590cd2bbc456f1098c6341b6f8260d0158bedea
Aron Kovacs <45430204+AronDev@users.noreply.github.com>
2019-11-29 16:38:15 +0100

    Update Functions.java

--- gyakorlas/Gyakorlas1_2019_11_28/Functions.java
+++ gyakorlas/Gyakorlas1_2019_11_28/Functions.java
@@ -2,49 +2,11 @@ package Gyakorlas1_2019_11_28;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
 import java.util.Scanner;
-import javax.swing.JOptionPane;
 
 public class Functions {    
-    static final int INFO_MSG = 0;
-    static final int WARN_MSG = 1;
-    static final int ERROR_MSG = 2;
-    static final int DEBUG_MSG = 3;
-    
-    static void showMsg(int msgType, String msgTitle, String msgText) {
-        JOptionPane.showMessageDialog(null, msgText, msgTitle, msgType);
-    }
-    
-    static void systemMsg(int msgType, String msgText) {
-        if(msgText.trim().length() > 0) {
-            switch(msgType) {
-                case 0: {
-                    System.out.printf("[I] %s\n", msgText);
-                    break;
-                } case 1: {
-                    System.out.printf("[W] %s\n", msgText);
-                    break;
-                } case 2: {
-                    System.out.printf("[E] %s\n", msgText);
-                    break;
-                } case 3: {
-                    if(Main.DEBUG) {
-                        System.out.printf("[D] %s\n", msgText);
-                    }
-                    break;
-                } default: {
-                    systemMsg(ERROR_MSG, String.format("Hibás msgType érték a systemMsg funkcióban! (msgType = %d)", msgType));
-                }
-            }
-        } else {
-            systemMsg(ERROR_MSG, "msgTextnek nincsenk értéke a systemMsg funkcióban!");
-        }
-    }
-    
     static void readFile(String fileName) {
-        systemMsg(DEBUG_MSG, String.format("Fájl beolvasása megkezdve.. (%s)", fileName));
+        System.out.println(String.format("Fájl beolvasása megkezdve.. (%s)", fileName));
         File f = new File(fileName);
         try(Scanner sc = new Scanner(f)) {
             String line;
@@ -58,9 +20,9 @@ public class Functions {
                     Datastores.sportagLista.get(Datastores.sportagLista.size() - 1).donto[i-1] = Integer.parseInt(elements[i]);
                 }
             }
-            systemMsg(DEBUG_MSG, String.format("Fájl beolvasva! (%s)", fileName));
+            System.out.println(String.format("Fájl beolvasva! (%s)", fileName));
         } catch(FileNotFoundException fnfe) {
-            systemMsg(ERROR_MSG, String.format("%s", fnfe.getMessage()));
+            System.err.println(fnfe.getMessage());
         }
     }
 }
